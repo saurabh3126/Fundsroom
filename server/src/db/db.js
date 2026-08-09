86817c01ef71eda0e30f1c67c400093e43ce4e6f
Saurbh 026 <saurabh239goswami@gmail.com>
2026-08-10 02:22:47 +0530

    Prepare app for deployment

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,11 +2,44 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+
+    // Render PostgreSQL
+    // Local PostgreSQL fallback
+    connectionString:
+        process.env.DATABASE_URL || undefined,
+
+    host:
+        process.env.DATABASE_URL
+            ? undefined
+            : process.env.DB_HOST,
+
+    port:
+        process.env.DATABASE_URL
+            ? undefined
+            : process.env.DB_PORT,
+
+    user:
+        process.env.DATABASE_URL
+            ? undefined
+            : process.env.DB_USER,
+
+    password:
+        process.env.DATABASE_URL
+            ? undefined
+            : process.env.DB_PASSWORD,
+
+    database:
+        process.env.DATABASE_URL
+            ? undefined
+            : process.env.DB_NAME,
+
+    // Required for hosted PostgreSQL connections
+    ssl:
+        process.env.DATABASE_URL
+            ? {
+                rejectUnauthorized: false
+            }
+            : false
 });
 
 module.exports = pool;
